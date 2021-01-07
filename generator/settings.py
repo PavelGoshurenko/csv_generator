7b41b594565604e6d2e82f88493bc06a4bbbd03c
PavelGoshurenko <goshurenkopl@gmail.com>
2021-01-07 23:38:01 +0200

    deploy40

--- generator/settings.py
+++ generator/settings.py
@@ -129,6 +129,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
