cee228cda7cd56bccaf0d32c5fc55cb15169ca75
PavelGoshurenko <goshurenkopl@gmail.com>
2021-01-07 20:19:58 +0200

    deploy28

--- generator/settings.py
+++ generator/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DEBUG', False))
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['csvfakegenerator.herokuapp.com', '127.0.0.1']
 
