aa6a6a0c969a964160505a013b7c45d517f8f0fc
PavelGoshurenko <goshurenkopl@gmail.com>
2021-01-07 19:44:38 +0200

    deploy24

--- csv_generator/tasks.py
+++ csv_generator/tasks.py
@@ -21,12 +21,12 @@ GENERATORS = {
     'Address': fake.address,
     'Phone number': fake.phone_number,
 }
-
+"""
 #if settings.DEBUG:
 #app.conf.update(BROKER_URL=os.environ['REDIS_URL'], CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
     #app.conf.broker_url = 'redis://localhost:6379/0'
 #else:
-    """ app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+     app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
                 CELERY_RESULT_BACKEND=os.environ['REDIS_URL']) """
 
 @app.task
